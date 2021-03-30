4ee86ebf2e9a8755345ae9114b4025e36cf87b0f
Tom Tarpey <tomtarpeydev@gmail.com>
2021-03-30 19:02:50 +0100

    django day 2

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
@@ -7,4 +8,8 @@ class Note(models.Model): # Table in a database
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
